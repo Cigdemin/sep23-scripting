659321ea7f1c951d8f788b11829b9819eb86b311
cigdemin <cigmenci@hotmail.com>
2023-11-06 11:18:59 -0600

    if_age file added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+#This folder includes my scripting class homeworks and note I have been started at 3 Non 2023 with Esen Burkanov
+
 # sep23-scripting
---
+++ if_age.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# read -p "Please specify your age: " age 
+
+# if age is less than 12:
+#     print: Hello, future programmer
+
+# if age is bigger than 12 and less than 30:
+#     print: Hello, junior programmer
+
+# if age is between bigger than 30 and less than 65:
+#     print: Hello, senior programmer
+
+read -p "Please specify your age: " age
