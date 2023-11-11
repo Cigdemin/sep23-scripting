65811cbb05745e9f2163d38169b3a5fd9359b77e
cigdemin <cigmenci@hotmail.com>
2023-11-11 22:31:50 +0000

    updated with new files

---
+++ api-basic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# JSON -- Javascript Object Notation
+
+# info="$(curl https://f4idu2pd8h.execute-api.us-east-1.amazonaws.com/v1/info | jq -r .body)"
+# echo $info
+DROPLET_ID="384481288"
+
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $DO_TOKEN" \
+  "https://api.digitalocean.com/v2/droplets/$DROPLET_ID" | jq .droplet.networks.v4[0].ip_address
