400a55ac9dae4e9c16ab19c1f13e206951bd1d2d
cigdemin <cigmenci@hotmail.com>
2023-11-11 18:25:22 +0000

    web_builder.sh added

---
+++ web_builder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ $3 -le 0 ]; then
+    echo "Usage : $0 <file_path>"
+    exit 1
+fi
+
+file_path=$1
+for web in $(cat $file_path); do
+    name="$(echo $web | awk -F "/" '{ print $9}' | awk -F "." '{ print $1 }')"
+    wget $web -P /tmp/
+    unzip /tmp/$name.zip -d /tmp/
+    rm -rf /tmp/*.zip
+    mkdir /var/www/html/$name
+    mv /tmp/*$name*/* /var/www/html/$name/
+done
+
+
+# function_name() {
+#     commands
+# }
+
+# function function_name() {
+#     commands
+# }
